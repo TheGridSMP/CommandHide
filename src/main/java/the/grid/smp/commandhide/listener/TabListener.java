6120683b07d26c3c9820b2b40830f81ddf8356c6
Dr Theo <71725814+DrTheodor@users.noreply.github.com>
2024-03-17 18:10:34 +0300

    add command blocking, just for you, Sloth

--- src/main/java/the/grid/smp/commandhide/listener/TabListener.java
+++ src/main/java/the/grid/smp/commandhide/listener/TabListener.java
@@ -1,7 +1,9 @@
 package the.grid.smp.commandhide.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.TabCompleteEvent;
 import the.grid.smp.commandhide.CommandHide;
 import the.grid.smp.commandhide.data.Command;
@@ -21,6 +23,9 @@ public class TabListener implements Listener {
 
     @EventHandler
     public void onTab(TabCompleteEvent event) {
+        if (!(event.getSender() instanceof Player))
+            return;
+
         Group group = this.plugin.config().getGroup(event.getSender());
         List<String> completions = new ArrayList<>(event.getCompletions());
 
@@ -29,6 +34,7 @@ public class TabListener implements Listener {
         for (Command command : group.commands()) {
             if (command.context().equals("*") && command.operation() == Operation.SUBTRACT) {
                 completions.clear();
+                break;
             }
         }
 
@@ -42,5 +48,36 @@ public class TabListener implements Listener {
         }
 
         event.setCompletions(completions);
+    }
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+        Group group = this.plugin.config().getGroup(event.getPlayer());
+        boolean disallowed = false;
+
+        for (Command command : group.commands()) {
+            if (command.context().equals("*") && command.operation() == Operation.SUBTRACT) {
+                disallowed = true;
+                break;
+            }
+        }
+
+        for (Command command : group.commands()) {
+            String slashed = "/" + command.context();
+            boolean matches = event.getMessage().startsWith(slashed);
+
+            if (!matches)
+                continue;
+
+            if (disallowed && command.operation() == Operation.ADD) {
+                disallowed = false;
+                break;
+            } else if (!disallowed && command.operation() == Operation.SUBTRACT) {
+                disallowed = true;
+                break;
+            }
+        }
+
+        event.setCancelled(disallowed);
     }
 }
